7348dcbdbcbbac6ee822f4ea19a6f702bdef79c8
Farragua <83448280+Farragua@users.noreply.github.com>
2026-08-24 12:53:29 +0200

    Update main_vYahoo_semanal.py

--- main_vYahoo_semanal.py
+++ main_vYahoo_semanal.py
@@ -103,7 +103,7 @@ def main_vYahoo_semanal():
         paths[key] = fp
 
     # === Elegir activo para evaluar ===
-    assets = ["MSFT", "GOOG", "META", "AMZN", "MA", "V", "ASTS", "SLS", "UBER", "MCD", "AENA.MC", "GC=F"]
+    assets = ["MSFT", "GOOG", "META", "AMZN", "MA", "V", "ASTS", "SLS", "UBER", "MCD", "AENA.MC", "GC=F", "SPY"]
 
     n_iteraciones = len(assets)
 
